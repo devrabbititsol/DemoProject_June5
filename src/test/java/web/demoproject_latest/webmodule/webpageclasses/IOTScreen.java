2e7bb4e2e946b1832d956d6da9abd3c075f4ec02
root <santhosh.annam@devrabbit.com>
2019-06-05 17:44:34 +0530

    Executing on Jenkins

--- src/test/java/web/demoproject_latest/webmodule/webpageclasses/IOTScreen.java
+++ src/test/java/web/demoproject_latest/webmodule/webpageclasses/IOTScreen.java
@@ -9,7 +9,7 @@ import com.relevantcodes.extentreports.ExtentTest;
 @SuppressWarnings("unused")
 public class IOTScreen extends BaseClass {
 	
-	public static String primaryInfo  = "{\"user_id\":7,\"is_generate\":false,\"is_execute\":false,\"is_web\":true,\"project_url\":\"https://www.devrabbit.com/\",\"report_upload_url\":\"https://smartqe.io:443/UploadReportFile\",\"project_name\":\"DemoProject_Latest\",\"project_description\":\"desc\",\"project_id\":296,\"module_name\":\"WebModule\",\"module_description\":\"desc\",\"sub_module_id\":0,\"module_id\":435,\"testcase_name\":\"ContactUsTestcase\",\"testcase_id\":481,\"testset_id\":0,\"executed_timestamp\":575550924,\"browser_type\":\"chrome\"}";
+	public static String primaryInfo  = "{\"user_id\":7,\"is_generate\":false,\"is_execute\":false,\"is_web\":true,\"project_url\":\"https://www.devrabbit.com/\",\"report_upload_url\":\"https://smartqe.io:443/UploadReportFile\",\"project_name\":\"DemoProject_Latest\",\"project_description\":\"desc\",\"project_id\":296,\"module_name\":\"WebModule\",\"module_description\":\"desc\",\"sub_module_id\":0,\"module_id\":435,\"testcase_name\":\"ContactUsTestcase\",\"testcase_id\":481,\"testset_id\":0,\"executed_timestamp\":663743303,\"browser_type\":\"chrome\"}";
 
 	public static String projectName = "demoproject_latest";
 	public WebDriver driver;
